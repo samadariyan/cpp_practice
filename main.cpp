f97fe62802253d73a54ec7d9611db540c03e8a3b
samadariyan <samadariyan74@gmail.com>
2025-12-03 02:24:29 +0330

    replace fmt with std

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
-/*
-    . Explore the format library in C++20 and C++23
-*/
 
-// #include <format>
-// #include <print>
-#include <fmt/format.h>
-#include <iostream>
 
 import utilities;
 
-int main() {
-  // standard_format_facilities();
-  external_format_facilities();
-}
+int main() { standard_format_facilities(); }
