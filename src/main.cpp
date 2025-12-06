afbb6cd892c2d7e2f0fc0f136f6d9d93728c8950
Samad Ariyan <samadariyan74@gmail.com>
2025-12-06 16:12:41 +0330

    string array

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,19 @@ import fortune_tellter;
 
 int main() {
 
+  // app();
+  // declare_and_initialize();
+  // construction();
+  // access_and_modify();
+  // appending_and_concatenation();
+  // substrings_and_finding();
+  // comparison();
+  // length_capacity_clear(););
   // it_1::app();
   // it_2::app();
   // it_3::app();
   // it_4::app();
   it_5::app();
+
   return 0;
 }
