98189dbeb99f1221abd0361b9ce23deadac19992
samadariyan <samadariyan74@gmail.com>
2026-01-07 10:57:50 +0330

    prepare challenge 2

--- src/main.cpp
+++ src/main.cpp
@@ -392,12 +392,20 @@ int main() {
 
 */
 
+/*
+
+
 int main() {
 
     // it_1::app();
     it_2::app();
 }
 
-/*
-
 */
+
+int main(int argc, char* argv[]) {
+
+    it_1::app();
+    // it_2::app();
+    // it_3::app(argc, argv);
+}
